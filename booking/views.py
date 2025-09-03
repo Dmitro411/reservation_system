e9134acd0c063c4f35ecfaf5ca2f391519df9d7f
Dmitro411 <dk0734303536@gmail.com>
2025-09-03 19:34:46 +0300

    add views for houses

---
+++ booking/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import House
+
+def index(request):
+    houses = House.objects.all()
+    return render(request, "index.html", context={"houses": houses})
+
+def house_detail(request, pk):
+    house = House.objects.get(id=pk)
+    return render(request, "house_detail.html", context={"house": house})
+
+
